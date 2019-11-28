13f75573ceea901d1b9737a21e2ebe61cc0c8bfe
maplecomdori <leeseunghoon88@gmail.com>
2019-11-27 21:52:32 -0500

    data manager

---
+++ app/src/main/java/com/example/yejoonmanual/DataManager.kt
@@ -0,0 +1,16 @@
+package com.example.yejoonmanual
+
+object DataManager {
+    val sectionTaskMap = HashMap<String, Array<String>>()
+
+
+    init {
+        val juboTasks = arrayOf("단축키", "아이디 & 비번", "예배 순서", "큐티",
+            "종이 접는 기계", "프린트", "리뷰 리스트")
+        val serviceTasks = arrayOf("예배당 셋업", "성찬식", "자리 안내 요령")
+
+        sectionTaskMap.set("주보", juboTasks)
+        sectionTaskMap.set("예배", serviceTasks)
+
+    }
+}
