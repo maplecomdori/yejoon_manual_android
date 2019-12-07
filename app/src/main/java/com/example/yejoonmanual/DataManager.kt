f4df800bab4beb58737a0c0f6b40e14b087dee23
maplecomdori <leeseunghoon88@gmail.com>
2019-12-06 22:02:43 -0500

    map for shortcuts

--- app/src/main/java/com/example/yejoonmanual/DataManager.kt
+++ app/src/main/java/com/example/yejoonmanual/DataManager.kt
@@ -1,7 +1,11 @@
 package com.example.yejoonmanual
 
+import java.util.*
+import kotlin.collections.HashMap
+
 object DataManager {
     val sectionTaskMap = HashMap<String, Array<String>>()
+    val shortcutMap = TreeMap<String, TreeMap<String, String>>()
 
 
     init {
@@ -12,5 +16,45 @@ object DataManager {
         sectionTaskMap.set("주보", juboTasks)
         sectionTaskMap.set("예배", serviceTasks)
 
+        prepareShortcuts()
     }
+
+    private fun prepareShortcuts() {
+        prepareTextSelectionShortcuts()
+        prepareCopyPasteShortcuts()
+        prepareFontShortcuts()
+    }
+
+    private fun prepareTextSelectionShortcuts() {
+        val textMap = TreeMap<String, String>()
+        textMap.put("전체 선택", "Ctrl + A")
+        textMap.put("저장", "Ctrl + S")
+        textMap.put("키보드로 텍스트 선택", "Shift + 방향키 / Home / End")
+        textMap.put("광고 댓글 전체 선택", "마우스 세 번 클릭")
+        textMap.put("전체 선택", "Ctrl + A")
+        textMap.put("전체 선택", "Ctrl + A")
+        shortcutMap.put("텍스트 선택", textMap)
+    }
+
+    private fun prepareCopyPasteShortcuts() {
+        val copyPasteMap = TreeMap<String, String>()
+        copyPasteMap.put("복사", "Ctrl + C")
+        copyPasteMap.put("붙이기", "Ctrl + V")
+        copyPasteMap.put("Paste Special\nUnformatted Text", "Ctrl + Alt + V")
+        shortcutMap.put("복사 & 붙이기", copyPasteMap)
+    }
+
+    private fun prepareFontShortcuts() {
+        val fontMap = TreeMap<String, String>()
+        fontMap.put("Bold", "Ctrl + B")
+        fontMap.put("Underline", "Ctrl + U")
+        shortcutMap.put("폰트", fontMap)
+    }
+
+    private fun prepareParagraphShortcuts() {
+        val paragraphMap = TreeMap<String, String>()
+        paragraphMap.put("양쪽 맞춤 (Justify)", "Ctrl + J")
+        shortcutMap.put("문단 정렬", paragraphMap)
+    }
+
 }
