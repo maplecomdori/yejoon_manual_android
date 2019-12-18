99e16d80c2992d5c08b713452fea69cb445d561d
maplecomdori <leeseunghoon88@gmail.com>
2019-12-18 10:29:26 -0500

    data class for instruction

---
+++ app/src/main/java/com/example/yejoonmanual/DataClass.kt
@@ -0,0 +1,4 @@
+package com.example.yejoonmanual
+
+// For instructions that comprises of title, picture, and description
+data class Instruction (val title: String, val picturePath: String, var description: String)
