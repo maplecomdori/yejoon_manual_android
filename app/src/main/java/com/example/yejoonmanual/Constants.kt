20ab18eb2389f20adb8dd3f99f2be2da5a7d3b54
maplecomdori <leeseunghoon88@gmail.com>
2019-12-24 12:37:43 -0500

    remove hardcoding

--- app/src/main/java/com/example/yejoonmanual/Constants.kt
+++ app/src/main/java/com/example/yejoonmanual/Constants.kt
@@ -6,3 +6,14 @@ const val JUBO_SECTION = "JUBO_SECTION"
 const val YEBE_SECTION = "YEBE_SECTION"
 const val POSITION_NOT_SET = -1
 
+// INSTRUCTION
+const val INST_SHORTCUT = "단축키"
+const val INST_CRED = "아이디 & 비번"
+const val INST_SERVICE_ORDER = "예배 순서"
+const val INST_QT = "큐티"
+const val INST_FOLDING_MACHINE = "종이 접는 기계"
+const val INST_PRINT = "프린트"
+const val INST_REVIEW = "리뷰 리스트"
+const val INST_YEBEDANG = "예배당 셋업"
+const val INST_COMMUNION = "성찬식"
+const val INST_SEATING_TIP = "자리 안내 요령"
--- app/src/main/java/com/example/yejoonmanual/DataManager.kt
+++ app/src/main/java/com/example/yejoonmanual/DataManager.kt
@@ -5,21 +5,33 @@ import kotlin.collections.HashMap
 
 object DataManager {
 
-    val juboTasks:Array<String> = arrayOf("단축키", "아이디 & 비번", "예배 순서", "큐티",
-        "종이 접는 기계", "프린트", "리뷰 리스트")
-    val serviceTasks:Array<String> = arrayOf("예배당 셋업", "성찬식", "자리 안내 요령")
-    val keyValueTasks:Set<String> = setOf("단축키", "아이디 & 비번")
-    val imageTasks:Set<String> = setOf("큐티", "종이 접는 기계", "프린트",
-        "예배당 셋업", "성찬식")
+    val juboTasks:Array<String> = arrayOf(
+        INST_SHORTCUT, INST_CRED, INST_SERVICE_ORDER, INST_QT,
+        INST_FOLDING_MACHINE, INST_PRINT, INST_REVIEW)
+    val serviceTasks:Array<String> = arrayOf(INST_YEBEDANG, INST_COMMUNION, INST_SEATING_TIP)
+    val keyValueTasks:Set<String> = setOf(INST_SHORTCUT, INST_CRED)
+    val imageTasks:Set<String> = setOf(INST_QT, INST_FOLDING_MACHINE, INST_PRINT,
+        INST_YEBEDANG, INST_COMMUNION)
 
     val keyValueTaskDataMap:TreeMap<String, Map<String, Map<String, String>>> = TreeMap()
     private val shortcutMap = TreeMap<String, Map<String, String>>()
     private val credMap = TreeMap<String, Map<String, String>>()
 
+    val foldingMachineInstructions:Array<Instruction> = prepareFoldingMachineInstructions()
 
     init {
         prepareShortcuts()
         prepareCredentials()
+    }
+
+    private fun prepareFoldingMachineInstructions(): Array<Instruction> {
+        val handle = Instruction("핸들 내리기", "images/folding_machine/folding_machine_handle.jpg", "손잡이를 내려 종이를 넣을 공간을 만들어요")
+        val paper = Instruction("종이 넣기", "images/folding_machine/folding_machine_paper.jpg", "민들레 포커스가 오른쪽에 놓이도록 종이를 넣고 손잡이를 다시 올려서 종이를 고정시켜 주세요")
+        val topScale = Instruction("위에 눈금 조절", "images/folding_machine/folding_machine_top.jpg", "상단 금속 부분의 밑 부분을 빨간선에 맞춰 주세요")
+        val bottomScale = Instruction("아래 눈금 조절", "images/folding_machine/folding_machine_bottom.jpg", "오른쪽은 '주보'라고 쓰여진 스티커와 금속에 붙은 화살표를 맞춰주세요. 왼쪽은 빨간선에 맞춰주세요.")
+        val testPrint = Instruction("테스트 프린트", "images/folding_machine/folding_machine_button.jpg", "TEST 버튼을 누르면 두 장이 접혀서 나와요. 잘 접혔으면 START 버튼을 눌러서 나머지를 접어주세요")
+        val cleanUp = Instruction("정리", "images/folding_machine/folding_machine_cleanup.jpg", "비닐을 덮고 전원을 끄고 전등을 끄고 나오세요")
+        return arrayOf(handle, paper, topScale, bottomScale, testPrint, cleanUp)
     }
 
     private fun prepareCredentials() {
@@ -27,7 +39,7 @@ object DataManager {
         prepareHomePageCredentials()
         prepareEmailCredentials()
         prepareQTCredentials()
-        keyValueTaskDataMap.put("아이디 & 비번", credMap)
+        keyValueTaskDataMap.put(INST_CRED, credMap)
     }
 
     private fun prepareLaptopCredentials() {
@@ -66,7 +78,7 @@ object DataManager {
         prepareCopyPasteShortcuts()
         prepareFontShortcuts()
         prepareParagraphShortcuts()
-        keyValueTaskDataMap.put("단축키", shortcutMap)
+        keyValueTaskDataMap.put(INST_SHORTCUT, shortcutMap)
     }
 
     private fun prepareTextSelectionShortcuts() {
